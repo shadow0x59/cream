df0357a42e90b71a5dab102142c31fe4a2e4678d
Raul Radu <raul.radu@mail.polimi.it>
2024-11-01 21:06:43 +0100

    [Code] Fixed API incogruency and added simplified calls to http methods

--- ExpressMiddleware/ExpressMiddleware.ts
+++ ExpressMiddleware/ExpressMiddleware.ts
@@ -1,81 +1,73 @@
-import { NextFunction, Request, Response } from 'express';
-import { RestError } from '../ExpressErrorHandler/ExpressErrorHandler';
+import {NextFunction, Request, Response} from 'express';
 
-export type MiddlewareDataCollection = {} | undefined;
+import {RestError} from '../ExpressErrorHandler/ExpressErrorHandler';
+
+export type MiddlewareDataCollection = {}|undefined;
 
 export type MiddlewareDataCollections = Map<String, MiddlewareDataCollection>;
 
 export interface ExtendedRequest extends Request {
-	middlewareDataCollections?: MiddlewareDataCollections;
+  middlewareDataCollections?: MiddlewareDataCollections;
 }
 
 export interface BaseMiddleware {
-	handle(
-		req: Request,
-		res: Response,
-		next: NextFunction
-	): void | Promise<void>;
+  handle(req: Request, res: Response, next: NextFunction): void|Promise<void>;
 }
 
 export abstract class AsyncExpressMiddleware<T> implements BaseMiddleware {
-	public abstract behaviour(
-		req: ExtendedRequest,
-		res: Response
-	): Promise<MiddlewareReturnData<T>>;
+  public abstract behaviour(req: ExtendedRequest, res: Response):
+      Promise<MiddlewareReturnData<T>>;
 
-	async handle(req: ExtendedRequest, res: Response, next: NextFunction) {
-		try {
-			let data: MiddlewareReturnData<T> = await this.behaviour(req, res);
-			let collections: MiddlewareDataCollections =
-				req.middlewareDataCollections || new Map();
-			if (data.content) {
-				collections.set(data.collectionName, data.content);
-			}
-			req.middlewareDataCollections = collections;
+  async handle(req: ExtendedRequest, res: Response, next: NextFunction) {
+    try {
+      let data: MiddlewareReturnData<T> = await this.behaviour(req, res);
+      let collections: MiddlewareDataCollections =
+          req.middlewareDataCollections || new Map();
+      if (data.content) {
+        collections.set(data.collectionName, data.content);
+      }
+      req.middlewareDataCollections = collections;
 
-			next();
-		} catch (e) {
-			if (e instanceof RestError) {
-				res.status((e as RestError).statusCode);
-			} else {
-				res.status(500);
-			}
-			next(e);
-		}
-	}
+      next();
+    } catch (e) {
+      if (e instanceof RestError) {
+        res.status((e as RestError).statusCode);
+      } else {
+        res.status(500);
+      }
+      next(e);
+    }
+  }
 }
 
 export abstract class ExpressMiddleware implements BaseMiddleware {
-	public abstract behaviour(
-		req: ExtendedRequest,
-		res: Response
-	): MiddlewareReturnData;
+  public abstract behaviour(req: ExtendedRequest, res: Response):
+      MiddlewareReturnData;
 
-	handle(req: ExtendedRequest, res: Response, next: NextFunction) {
-		try {
-			let data: MiddlewareReturnData = this.behaviour(req, res);
-			let collections: MiddlewareDataCollections =
-				req.middlewareDataCollections || new Map();
-			collections.set(data.collectionName, data.content);
-			req.middlewareDataCollections = collections;
+  handle(req: ExtendedRequest, res: Response, next: NextFunction) {
+    try {
+      let data: MiddlewareReturnData = this.behaviour(req, res);
+      let collections: MiddlewareDataCollections =
+          req.middlewareDataCollections || new Map();
+      collections.set(data.collectionName, data.content);
+      req.middlewareDataCollections = collections;
 
-			next();
-		} catch (e) {
-			if (e instanceof RestError) {
-				res.status((e as RestError).statusCode);
-			} else {
-				res.status(500);
-			}
-			next(e);
-		}
-	}
+      next();
+    } catch (e) {
+      if (e instanceof RestError) {
+        res.status((e as RestError).statusCode);
+      } else {
+        res.status(500);
+      }
+      next(e);
+    }
+  }
 }
 
 export type BaseMiddlewares = BaseMiddleware[];
 
 export class MiddlewareReturnData<T = {}> {
-	constructor(
-		public readonly content?: T,
-		public readonly collectionName = 'default'
-	) {}
+  constructor(
+      public readonly content?: T, public readonly collectionName = 'default') {
+  }
 }
--- ExpressMiddleware/MiddlewareData.ts
+++ ExpressMiddleware/MiddlewareData.ts
@@ -1,45 +1,25 @@
-import { ParameterProp } from '../ExpressAdapter/ParameterProp';
-import { MIDDLEWARE_METADATA_KEY } from '../ExpressAdapter/ExpressAdapters';
+import {MIDDLEWARE_METADATA_KEY} from '../ExpressAdapter/ExpressAdapters';
+import {ParameterProp} from '../ExpressAdapter/ParameterProp';
 
 export class MiddlewareParameterProp extends ParameterProp {
-	constructor(
-		index: number,
-		name: string,
-		public readonly collection: string
-	) {
-		super(index, name);
-	}
+  constructor(index: number, name: string, public readonly collection: string) {
+    super(index, name);
+  }
 }
 
 export type MiddlewareParameterProps = MiddlewareParameterProp[];
 
 export function MiddlewareData(
-	dataName: string = '$',
-	collectionName: string = 'default'
-) {
-	return function (
-		target: Object,
-		propertyKey: string | symbol,
-		parameterIndex: number
-	) {
-		let existingRequiredParameters: MiddlewareParameterProps =
-			Reflect.getOwnMetadata(
-				MIDDLEWARE_METADATA_KEY,
-				target,
-				propertyKey
-			) || [];
-		existingRequiredParameters.push(
-			new MiddlewareParameterProp(
-				parameterIndex,
-				dataName,
-				collectionName
-			)
-		);
-		Reflect.defineMetadata(
-			MIDDLEWARE_METADATA_KEY,
-			existingRequiredParameters,
-			target,
-			propertyKey
-		);
-	};
+    collectionName: string = 'default', dataName: string = '$') {
+  return function(
+      target: Object, propertyKey: string|symbol, parameterIndex: number) {
+    let existingRequiredParameters: MiddlewareParameterProps =
+        Reflect.getOwnMetadata(MIDDLEWARE_METADATA_KEY, target, propertyKey) ||
+        [];
+    existingRequiredParameters.push(
+        new MiddlewareParameterProp(parameterIndex, dataName, collectionName));
+    Reflect.defineMetadata(
+        MIDDLEWARE_METADATA_KEY, existingRequiredParameters, target,
+        propertyKey);
+  };
 }
